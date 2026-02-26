51734de821221a91d9a993e8bacb6ef0e74820c5
nabanita29-web <nabachowdhury2@gmail.com>
2026-02-26 14:58:35 +0530

    ch

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ app.get('/checkdata', async (req, res) => {
 })
 app.get('/taketodos', async (req, res) => {
     const id = req.query.e;
-    const todo = (await Todo.query(`select * from todos where id=$1`,[id]));
+    const todo = (await Todo.query(`select * from todofor where id=$1`,[id]));
     // await Todo.query(`DELETE FROM todos`)
-    res.send(todo.rows);
+    res.send(todo);
 
 })
 //------------------------
