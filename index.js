8650abc7f40a58a5953e7c876789a102a45c946e
nabanita29-web <nabachowdhury2@gmail.com>
2026-02-26 20:06:39 +0530

    ch

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.put('/todone', async (req, res) => {
     // Todo.query(`INSERT INTO users DEFAULT VALUES`)
     const task = req.body.key1;
     const id = req.body.key2;
+    console.log(task);
     await Todo.query(`UPDATE todofor SET status='done' (WHERE task=$1 AND id=$2 AND dateof=$3)`,[task.task, id, task.date]);
     // var result = await Todo.query(`SELECT * FROM users`);
     // res.redirect(`/${rows}`)
